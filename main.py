b338ca616a23b4e78754712a96b5e4bf072b923a
yugsahal2010-hash <yug.sahal2010@gmail.com>
2026-04-16 10:33:06 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_methods=["*"], 
+    allow_headers=["*"], 
+)
 
 class MatchInput(BaseModel):
     current_score: int
